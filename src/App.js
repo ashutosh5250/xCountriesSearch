293fd8626f4fc53a5b3d20324b7ecf0b22bd1b69
Ashutosh kushire <ashutoshkushire97@gmail.com>
2024-03-30 13:07:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchCountries = async () => {
+    try {
+      const response = await axios.get("https://restcountries.com/v3.1/all");
+      setCountries(response.data);
+      setFilteredCountries(response.data);
+    } catch (error) {
+      console.error("Error fetching countries:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const handleSearch = (event) => {
+    const searchValue = event.target.value.toLowerCase();
+    setSearchTerm(searchValue);
+
+    const filtered = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(searchValue)
+    );
+    setFilteredCountries(filtered);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="input">
+        <input
+          type="text"
+          placeholder="Search for countries"
+          onChange={handleSearch}
+          value={searchTerm}
+        />
+      </div>
+      <div className="container">
+        {filteredCountries.map((country, index) => (
+          <div key={index} className="countryCard">
+            <img src={country.flags.svg} alt="Flag not available" />
+            <p>{country.name.common}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
